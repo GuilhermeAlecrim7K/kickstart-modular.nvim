556f370eba760d346c4e33e7b583610d177f72e1
Guilherme Alecrim <guilhermealecrim96@gmail.com>
2024-03-09 19:03:09 -0300

    Remove which-key plugin

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -30,8 +30,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/gitsigns',
 
-  require 'kickstart/plugins/which-key',
-
   require 'kickstart/plugins/telescope',
 
   require 'kickstart/plugins/lspconfig',
